e8a561c7e0aba7be3a35ee7278d5b93d8fdd8841
NickLacunha <njpadinha@gmail.com>
2017-05-10 14:05:54 -0700

    templating routes

---
+++ components/NotFoundPage.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react');
+var Link = require('react-router/Link');
+
+export default class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div className="not-found">
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+        <p>
+          <Link to="/">Go back to the main page</Link>
+        </p>
+      </div>
+    );
+  }
+}
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var path = require('path')
 var express = require('express')
 var app = express()
 var React = require('react');
+var renderToString = require('react-dom/server/renderToString');
+var match = require('react-router/match');
+var RouterContext = require ('react-router/RouterContext');
+var routes = require('./routes');
+var NotFoundPage = require('.components/NotFoundPage');
+
 
 // initialize app
 app.set('view engine', 'ejs');
@@ -32,6 +38,35 @@ Home page
 */
 app.get('/home/', function (req, res) {
     // serve up the react-router spa
+    match(
+    { routes, location: req.url },
+    (err, redirectLocation, renderProps) => {
+
+      // in case of error display the error message
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+
+      // in case of redirect propagate the redirect to the browser
+      if (redirectLocation) {
+        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      }
+
+      // generate the React markup for the current route
+      let markup;
+      if (renderProps) {
+        // if the current route matched we have renderProps
+        markup = renderToString(<RouterContext {...renderProps}/>);
+      } else {
+        // otherwise we can render a 404 page
+        markup = renderToString(<NotFoundPage/>);
+        res.status(404);
+      }
+
+      // render the index template with the embedded React markup
+      return res.render('index', { markup });
+    }
+  );
 })
 
 app.listen(3000, function () {
