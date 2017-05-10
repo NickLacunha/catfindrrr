8508b6844b1dd8ca72fb51a28f13d66a87a52be6
NickLacunha <njpadinha@gmail.com>
2017-05-10 13:56:28 -0700

    added views and public to folder structure
    updated server.js accordingly

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+var path = require('path')
 var express = require('express')
 var app = express()
+
+// initialize app
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(_dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
