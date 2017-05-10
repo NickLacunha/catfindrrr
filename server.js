4364c9efae4c433c86f62624778c3d65b74deff6
NickLacunha <njpadinha@gmail.com>
2017-05-10 13:45:42 -0700

    templating

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+/*
+c@Findrrr API
+*/
+app.get('/cats/random', function (req, res) {
+  // should return a random public ID from the Catabase
+    
+})
+
+app.post('/cats/', function (req, res) {
+  /*
+  should call the cloudinary api to add the file, then store
+  the public id in our mongo database so that we can retrieve it later  
+  */
+})
+
+/*
+Home page
+*/
+app.get('/home/', function (req, res) {
+    // serve up the react-router spa
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
