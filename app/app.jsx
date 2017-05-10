e8000ebacbf9b35c8c17863935c8099e6ed5a1eb
NickLacunha <njpadinha@gmail.com>
2017-05-09 18:38:40 -0700

    testing cloudinary integration

--- app/app.jsx
+++ app/app.jsx
@@ -33,6 +33,7 @@ var Home = React.createClass({
     </p>
 
     <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+    <img src="http://res.cloudinary.com/the-lacunha-group/image/upload/v1494379746/IMG_2484_dietcn.jpg" />
 </div>
     );
     }
