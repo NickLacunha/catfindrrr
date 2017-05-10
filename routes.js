86ae669268d0d1ece14c88ca93132eee17a03ac9
NickLacunha <njpadinha@gmail.com>
2017-05-10 14:11:30 -0700

    More setup/readying

--- routes.js
+++ routes.js
@@ -2,17 +2,21 @@
 
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
+/*
+TODO
+Replace these w/ yr own components (nameley the ones that are down there in the routes variable)
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import AthletePage from './components/AthletePage';
 import NotFoundPage from './components/NotFoundPage';
+*/
 
 const routes = (
-  <Route path="/" component={Layout}>
-    <IndexRoute component={IndexPage}/>
-    <Route path="athlete/:id" component={AthletePage}/>
-    <Route path="*" component={NotFoundPage}/>
-  </Route>
+    <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="stuff" component={SubmitCat} />
+        <Route path="contact" component={Contact} />
+    </Route>
 );
 
 export default routes;
