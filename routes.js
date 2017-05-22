cc69524ec83bb5a3b028184a8b3a4882869899a3
NickLacunha <njpadinha@gmail.com>
2017-05-22 13:16:06 -0700

    added home component to main page

--- routes.js
+++ routes.js
@@ -4,15 +4,15 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import {Layout} from './components/Layout';
-/*import Home from './components/Home';
-import SubmitCat from './components/SubmitCat';
+import Home from './components/Home';
+/*import SubmitCat from './components/SubmitCat';
 import Contact from './components/Contact';*/
 import {NotFoundPage} from './components/NotFoundPage';
 
 
 const routes = (
     <Route path="/" component={Layout}>
-
+        <IndexRoute component={Home}/>
     </Route>
 );
 
