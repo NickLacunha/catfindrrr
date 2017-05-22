2d8ad25c0bd01d0af64076268bb9d2d9e38d9be2
NickLacunha <njpadinha@gmail.com>
2017-05-22 13:10:16 -0700

    finishing up some things before trying to get webpack ready
    realized mistake w/ static routing and moved on

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ console.log(match);
 
 // initialize app
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(Express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+app.use(Express.static(path.join(__dirname, '/public')));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
